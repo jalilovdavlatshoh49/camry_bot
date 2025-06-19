d4fbcfecf711819703b2c9cce8a14967799e8e45
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2025-06-19 14:02:20 +0500

    Update dispatcher.py

--- bot/dispatcher.py
+++ bot/dispatcher.py
@@ -1,20 +1,13 @@
+# bot/bot.py
 import os
 from aiogram import Bot, Dispatcher
-from bot.routers import user, admin
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
 
 load_dotenv()
 
-# Получение токена бота из переменной окружения
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не установлен в .env файле!")
 
-# Инициализация бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-
-# Подключение маршрутизаторов
-dp.include_router(user.router)
-dp.include_router(admin.router
